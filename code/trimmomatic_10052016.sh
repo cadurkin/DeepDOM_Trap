26239ae2636a9d9dd0af5f71be33ab76b532aef3
Harriet Alexander <harriet.xander@gmail.com>
2016-10-05 16:47:32 -0700

    Update trimmomatic_10052016.sh
    Updated note about trimming for JGI

--- code/trimmomatic_10052016.sh
+++ code/trimmomatic_10052016.sh
@@ -2,6 +2,7 @@
 
 #This code is for trimming DeepDOM 150bp paired reads using Trimmomatic 0.32
 #usage: trimmomatic_10052016.sh forward.fastq.gz reverse.fastq.gz
+#Note: JGI sequences were already trimmed
 
 TAIL='.fastq.gz'
 SE_TAIL='_SE.trim.fastq.gz'
